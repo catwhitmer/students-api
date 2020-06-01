e3376347fa792b82ea3db1a4a5a336618760b82a
catwhitmer <catwhitmer@gmail.com>
2020-06-01 13:40:36 -0400

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.create(name: "Harry Potter", house: "Gryffindor", blood_status: "Half Blood", patronus: "Stag")
+Student.create(name: "Hermione Granger", house: "Gryffindor", blood_status: "Muggle Born", patronus: "Otter")
+Student.create(name: "Ron Weasley", house: "Gryffindor", blood_status: "Pure Blood", patronus: "Jack Russell terrier")
+Student.create(name: "Dean Thomas", house: "Gryffindor", blood_status: "Half Blood", patronus: "Wolf")
+
+Student.create(name: "Luna Lovegood", house: "Ravenclaw", blood_status: "Pure Blood", patronus: "Hare")
+Student.create(name: "Cho Chang", house: "Ravenclaw", blood_status: "Pure Blood", patronus: "Swan")
+Student.create(name: "Padma Patil", house: "Ravenclaw", blood_status: "Half Blood", patronus: "Tiger")
+Student.create(name: "Miachael Corner", house: "Ravenclaw", blood_status: "Half Blood", patronus: "Owl")
+
+Student.create(name: "Cedric Diggory", house: "Hufflepuff", blood_status: "Pure Blood", patronus: "Golden Retriever")
+Student.create(name: "Ernie Macmillan", house: "Hufflepuff", blood_status: "Pure Blood", patronus: "Boar")
+Student.create(name: "Hannah Abbott", house: "Hufflepuff", blood_status: "Half Blood", patronus: "Kitten")
+Student.create(name: "Susan Bones", house: "Hufflepuff", blood_status: "Half Blood", patronus: "Goat")
+
+Student.create(name: "Blaise Zabini", house: "Slytherin", blood_status: "Pure Blood", patronus: "Bear")
+Student.create(name: "Tracy Davis", house: "Slytherin", blood_status: "Half Blood", patronus: "Beetle")
+Student.create(name: "Millicent Bulstrode", house: "Slytherin", blood_status: "Half Blood", patronus: "Bullfrog")
+Student.create(name: "Draco Malfoy", house: "Slytherin", blood_status: "Pure Blood", patronus: "none")
+
