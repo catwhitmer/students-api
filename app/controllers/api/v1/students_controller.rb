d122cffb76bd06e14ad996f062f3dac91e070cb6
catwhitmer <catwhitmer@gmail.com>
2020-06-01 13:34:37 -0400

    added the index and show actions to the students controller

--- app/controllers/api/v1/students_controller.rb
+++ app/controllers/api/v1/students_controller.rb
@@ -1,2 +1,13 @@
 class Api::V1::StudentsController < ApplicationController
+  def index
+    @student = Student.all 
+
+    render json: @students, status: 200
+  end
+
+  def show 
+    @student = Student.find_by(params [:id])
+
+    render json: @student, status: 200
+  end
 end
