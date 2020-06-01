e1e9d49378b0c61a7558c75955b1027bd70fef5a
catwhitmer <catwhitmer@gmail.com>
2020-06-01 13:23:35 -0400

    added the Student migration

---
+++ db/migrate/20200601171909_create_students.rb
@@ -0,0 +1,12 @@
+class CreateStudents < ActiveRecord::Migration[6.0]
+  def change
+    create_table :students do |t|
+      t.string :name
+      t.string :house
+      t.string :blood_status
+      t.string :patronus
+
+      t.timestamps
+    end
+  end
+end
