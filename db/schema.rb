24b47c140dca48c6e61e1a0f885fa02fb45b1e10
catwhitmer <catwhitmer@gmail.com>
2020-06-02 18:21:36 -0400

    added teacher migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_01_171909) do
+ActiveRecord::Schema.define(version: 2020_06_02_221701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 2020_06_01_171909) do
     t.string "house"
     t.string "blood_status"
     t.string "patronus"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "teachers", force: :cascade do |t|
+    t.string "name"
+    t.integer "student_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
